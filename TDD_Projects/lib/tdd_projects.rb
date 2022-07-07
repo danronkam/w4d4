b2fe1136e5f6c19791b7f8aefdcb83332e789898
AA Student <appacademy@Mini-7.local>
2022-07-07 15:09:20 -0700

    TDD DONE

--- TDD_Projects/lib/tdd_projects.rb
+++ TDD_Projects/lib/tdd_projects.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class Array
 
     def uniq
@@ -23,5 +24,104 @@ class Array
         pairs
     end
 
-    
+    def my_transpose
+        new_arr = Array.new(self.length) {Array.new(self.length)}
+
+        self.each_with_index do |subArr, i|
+            subArr.each_with_index do |ele, j|
+                new_arr[j][i] = ele
+            end
+        end
+           
+        new_arr
+    end
+
+    def stock_picker
+        pair = []
+        biggest_profit = nil
+
+        self.each_with_index do |ele, i|
+            buy_price = ele
+
+            self.each_with_index do |ele2, j|
+                price_change = ele2 - ele
+
+                if biggest_profit == nil || price_change > biggest_profit
+                    biggest_profit = price_change
+                    pair = [i,j]
+                end
+            end
+        end
+        pair
+    end
+
 end
+
+class Towers_of_Hanoi
+    attr_reader :size, :board
+
+    def initialize(size)
+        @size = size
+        @board = Array.new(3) { Array.new }
+        self.populate
+    end
+
+    def populate
+        # disk 1 is the top
+        @board[0] = (1..size).to_a
+    end
+
+    def get_move
+        # print "Enter a starting tower: "
+        # start_pile = gets.chomp.to_i
+        # print "Enter an ending tower: "
+        # end_pile = gets.chomp.to_i
+        # [start_pile, end_pile] 
+        print "enter a starting and ending tower: "
+        gets.chomp.split.map(&:to_i)
+    end
+
+    def won?
+        @board.last == (1..size).to_a
+    end
+
+    def valid_move?(pos)
+        start_pile, end_pile = pos
+        # compare the top disks of each pile
+        # first check if the ending pile is empty 
+        # if start_pile's disk > end_pile's disk, then return false
+        return true if @board[end_pile].first.nil? 
+        @board[start_pile].first < @board[end_pile].first
+    end
+
+    # to move a disk from one tower to another
+    # shift first disk from start pile, unshift to the end pile
+    def move(pos)
+        start_pile, end_pile = pos
+        @board[end_pile].unshift(@board[start_pile].shift)
+    end
+
+    def render
+        @board.each_with_index do |subArr, i|
+            print "Top of Tower #{i}: "
+            puts subArr.join(" ")
+        end
+    end
+
+    def play
+        while !self.won?
+            self.render
+            input = self.get_move
+            while !self.valid_move?(input)
+                puts "Invalid Move"
+                input = self.get_move
+            end
+            move(input)
+        end
+        puts "You win"
+    end
+
+end
+
+# game = Towers_of_Hanoi.new(1)
+# game.play
--- TDD_Projects/spec/tdd_projects_spec.rb
+++ TDD_Projects/spec/tdd_projects_spec.rb
@@ -24,4 +24,63 @@ describe Array do
             expect(array.two_sum).to eq([[0,4], [2, 3]])
         end
     end
+
+    describe '#my_transpose' do 
+        rows = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8]
+        ]
+
+        it 'convert between the row-orientated and colum-oriented represenations' do
+            expect(rows.my_transpose).to eq([
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8]
+              ])
+        end
+    end
+
+    describe '#stock_picker' do 
+        array = [14, 17, 31, 25, 18, 15, 19]
+
+        it 'returns the most profitable pair of days to buy then sell stock' do
+            expect(array.stock_picker).to  eq([0,2])
+
+
+        end
+
+
+    end 
+end
+
+describe Towers_of_Hanoi do
+    subject(:towers) {Towers_of_Hanoi.new(3)}
+    describe '#initialize' do
+        it 'should receive a size' do 
+            expect(towers.size).to be(3)
+        end
+
+        it 'should create a board' do
+            board = Array.new(3) { Array.new }
+            board[0] = [1,2,3]
+            expect(towers.board).to eq(board)
+        end
+
+        it 'should populate the board' do 
+            expect(towers.board[0]).to eq([1,2,3])
+        end
+    end
+
+    describe 'get_move' do 
+        it 'should get an input' do 
+            input = double("0 1\n", :chomp=>"0 1")
+            allow(towers).to receive(:gets).and_return(input)
+
+            expect(input).to receive(:chomp)
+            expect(towers).to receive(:gets)
+            towers.get_move
+        end
+    end
+
 end
